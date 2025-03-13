fdaf14e8b9d94ad2e7059bd1a48195702f0288a3
sadhin28 <taosifsadhin113@gmail.com>
2025-03-13 21:44:45 +0600

    scipt add

---
+++ script.js
@@ -0,0 +1,7 @@
+ alert("Welcome Back")
+ const navToggle = document.querySelector('nav-toggle');
+ navToggle.addEventListener('click', navTogo);
+ function navToggle(){
+     console.log('hi')
+ }
+ 
