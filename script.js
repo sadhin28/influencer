e3017cd752a6955ffc346560587d98564ad3ec57
sadhin28 <taosifsadhin113@gmail.com>
2025-03-13 22:21:24 +0600

    javascript implement

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
- alert("Welcome Back")
- const navToggle = document.querySelector('nav-toggle');
- navToggle.addEventListener('click', navTogo);
- function navToggle(){
-     console.log('hi')
- }
- 
+
+const navToggle = document.querySelector('.nav-toggle');
+navToggle.addEventListener('click', navTogo);
+function navTogo(){
+    console.log('Hi')
+}
